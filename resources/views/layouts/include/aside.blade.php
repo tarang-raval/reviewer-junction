8b8b6c84d8fcda7a84383177151f946455f98647
Tarang Raval <tarangraval.developer@gmail.com>
2022-09-21 10:02:18 +0530

    Update aside.blade.php

--- resources/views/layouts/include/aside.blade.php
+++ resources/views/layouts/include/aside.blade.php
@@ -86,7 +86,7 @@
             </a>
           </li>
           <li class="nav-item">
-            <a href="{{ route('admin.user.index') }}" class="nav-link {{ (request()->is('admin/user/*')) ? 'active' : '' }}">
+            <a href="{{ route('admin.setting.index') }}" class="nav-link {{ (request()->is('admin/setting/*')) ? 'active' : '' }}">
                 <i class="nav-icon fas fa-cog"></i>
               <p>
              Setting
